588b8f2d0d2984ca1cca614f975ba775d46f2e3e
theruziev2004 <ruzievrustam58@yandex.ru>
2021-02-01 16:42:06 +0500

    Удачи))

--- js/popup.js
+++ js/popup.js
@@ -1,31 +1,33 @@
 let popUp = document.querySelector('.popup');
-
 let editForm = document.querySelector('.edit-form');
 let formCloseButton = editForm.querySelector('.edit-form__close-button');
-let inputName = editForm.querySelector('.edit-form__input-name');
-let inputCaption = editForm.querySelector('.edit-form__input-caption');
+let inputName = document.querySelector('#profile-name');
+let inputCaption = document.querySelector('#profile-caption');
 
 let profile = document.querySelector('.profile');
 let profileEditButton = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__name');
 let profileCaption = profile.querySelector('.profile__caption');
 
-function togglePopUp(evt) {
-    evt.preventDefault();
-    popUp.classList.toggle('popup_opened');
-    inputName.focus();
+function openPopUp(evt) {
+    popUp.classList.add('popup_opened');
     inputName.value = profileName.textContent;
     inputCaption.value = profileCaption.textContent;
 }
+
+function closePopUp(evt) {
+    popUp.classList.remove('popup_opened');
+}
+
 
 function saveForm(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileCaption.textContent = inputCaption.value;
-    togglePopUp(evt);
+    closePopUp(evt);
 }
 
 
-profileEditButton.addEventListener('click', togglePopUp);
-formCloseButton.addEventListener('click', togglePopUp);
+profileEditButton.addEventListener('click', openPopUp);
+formCloseButton.addEventListener('click', closePopUp);
 editForm.addEventListener('submit', saveForm);
